d9e114412b4a718a5f2fb5df5b1350311d16c788
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-05-17 14:45:14 +0100

    moved already cuda-fied functions back to canny.cu

--- cuda-canny/src/canny.cu
+++ cuda-canny/src/canny.cu
@@ -16,6 +16,140 @@
 #include "canny-device.cu"
 //==============================
 
+// convolution of in image to out image using kernel of kn width
+void convolution(const pixel_t *in, pixel_t *out, const float *kernel,
+                 const int nx, const int ny, const int kn)
+{
+    assert(kn % 2 == 1);
+    assert(nx > kn && ny > kn);
+    const int khalf = kn / 2;
+
+    for (int m = khalf; m < nx - khalf; m++)
+        for (int n = khalf; n < ny - khalf; n++)
+        {
+            float pixel = 0.0;
+            size_t c = 0;
+            for (int j = -khalf; j <= khalf; j++)
+                for (int i = -khalf; i <= khalf; i++)
+                {
+                    pixel += in[(n - j) * nx + m - i] * kernel[c];
+                    c++;
+                }
+
+            out[n * nx + m] = (pixel_t)pixel;
+        }
+}
+
+// determines min and max of in image
+void min_max(const pixel_t *in, const int nx, const int ny, pixel_t *pmin, pixel_t *pmax)
+{
+    int min = INT_MAX, max = -INT_MAX;
+
+    for (int m = 0; m < nx; m++)
+        for (int n = 0; n < ny; n++)
+        {
+            int pixel = in[n * nx + m];
+            if (pixel < min)
+                min = pixel;
+            if (pixel > max)
+                max = pixel;
+        }
+    *pmin = min;
+    *pmax = max;
+}
+
+// normalizes inout image using min and max values
+void normalize(pixel_t *inout,
+               const int nx, const int ny, const int kn,
+               const int min, const int max)
+{
+    const int khalf = kn / 2;
+
+    for (int m = khalf; m < nx - khalf; m++)
+        for (int n = khalf; n < ny - khalf; n++)
+        {
+
+            pixel_t pixel = MAX_BRIGHTNESS * ((int)inout[n * nx + m] - (float)min) / ((float)max - (float)min);
+            inout[n * nx + m] = pixel;
+        }
+}
+
+/*
+ * gaussianFilter:
+ * http://www.songho.ca/dsp/cannyedge/cannyedge.html
+ * determine size of kernel (odd #)
+ * 0.0 <= sigma < 0.5 : 3
+ * 0.5 <= sigma < 1.0 : 5
+ * 1.0 <= sigma < 1.5 : 7
+ * 1.5 <= sigma < 2.0 : 9
+ * 2.0 <= sigma < 2.5 : 11
+ * 2.5 <= sigma < 3.0 : 13 ...
+ * kernelSize = 2 * int(2*sigma) + 3;
+ */
+void gaussian_filter(const pixel_t *in, pixel_t *out,
+                     const int nx, const int ny, const float sigma)
+{
+    const int n = 2 * (int)(2 * sigma) + 3;
+    const float mean = (float)floor(n / 2.0);
+    float kernel[n * n]; // variable length array
+
+    fprintf(stderr, "gaussian_filter: kernel size %d, sigma=%g\n",
+            n, sigma);
+    size_t c = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            kernel[c] = exp(-0.5 * (pow((i - mean) / sigma, 2.0) +
+                                    pow((j - mean) / sigma, 2.0))) /
+                        (2 * M_PI * sigma * sigma);
+            c++;
+        }
+
+    convolution(in, out, kernel, nx, ny, n);
+    pixel_t max, min;
+    min_max(out, nx, ny, &min, &max);
+    normalize(out, nx, ny, n, min, max);
+}
+
+
+// Canny non-maximum suppression
+void non_maximum_supression(const pixel_t *after_Gx, const pixel_t *after_Gy, const pixel_t *G, pixel_t *nms,
+                            const int nx, const int ny)
+{
+    for (int i = 1; i < nx - 1; i++)
+        for (int j = 1; j < ny - 1; j++)
+        {
+            const int c = i + nx * j;
+            const int nn = c - nx;
+            const int ss = c + nx;
+            const int ww = c + 1;
+            const int ee = c - 1;
+            const int nw = nn + 1;
+            const int ne = nn - 1;
+            const int sw = ss + 1;
+            const int se = ss - 1;
+
+            const float dir = (float)(fmod(atan2(after_Gy[c],
+                                                 after_Gx[c]) +
+                                               M_PI,
+                                           M_PI) /
+                                      M_PI) *
+                              8;
+
+            if (((dir <= 1 || dir > 7) && G[c] > G[ee] &&
+                 G[c] > G[ww]) || // 0 deg
+                ((dir > 1 && dir <= 3) && G[c] > G[nw] &&
+                 G[c] > G[se]) || // 45 deg
+                ((dir > 3 && dir <= 5) && G[c] > G[nn] &&
+                 G[c] > G[ss]) || // 90 deg
+                ((dir > 5 && dir <= 7) && G[c] > G[ne] &&
+                 G[c] > G[sw])) // 135 deg
+                nms[c] = G[c];
+            else
+                nms[c] = 0;
+        }
+}
+
 //canny edge detector code to run on the host
 void cannyHost( const int *h_idata, const int w, const int h,
                const int tmin,            // tmin canny parameter
