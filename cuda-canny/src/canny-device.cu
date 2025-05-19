764251c24a017c8b3687227f43ee3e0642af38f3
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-05-19 15:30:38 +0100

    I think we missed some points in parallel reduction

--- cuda-canny/src/canny-device.cu
+++ cuda-canny/src/canny-device.cu
@@ -192,7 +192,7 @@ __global__ void min_max_cuda(const pixel_t *in, const int nx, const int ny, pixe
     __syncthreads();
     
 
-    for(int s=blockDim.x/2; s>0;s>>=1){ //parallel reduction (if doing atomicmin/max has too much contention, we simply reduce the number of values)
+    for(int s=(blockDim.x * blockDim.y) / 2; s>0;s>>=1){ //parallel reduction (if doing atomicmin/max has too much contention, we simply reduce the number of values)
         if(tid < s){
             smin[tid]=min(smin[tid],smin[tid+s]);
             smax[tid]=max(smax[tid],smax[tid+s]);
