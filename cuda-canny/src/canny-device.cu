d17c7a0e75b42386ff5b3befc9d944364e795731
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-05-19 12:47:06 +0100

    only uncommented normalize

--- cuda-canny/src/canny-device.cu
+++ cuda-canny/src/canny-device.cu
@@ -257,7 +257,7 @@ void gaussian_filter_cuda(const pixel_t *in, pixel_t *out,
 
     min_max_cuda<<<grid,block,2*block.x*block.y*sizeof(pixel_t)>>>(out, nx, ny, d_min, d_max);
 
-    //normalize_cuda<<<grid,block>>>(out, nx, ny, n, d_min, d_max);
+    normalize_cuda<<<grid,block>>>(out, nx, ny, n, d_min, d_max);
 }
 
 __global__ void non_maximum_suppression_kernel(const pixel_t *after_Gx, const pixel_t *after_Gy, const pixel_t *G, pixel_t *nms, const int nx, const int ny)
