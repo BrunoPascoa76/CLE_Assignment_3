f1d9ed3a9bc775772ecff7a3e3af1ec9ce470b3d
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-05-19 12:26:54 +0100

    added min_max cuda back

--- cuda-canny/src/canny-device.cu
+++ cuda-canny/src/canny-device.cu
@@ -233,7 +233,7 @@ void gaussian_filter_cuda(const pixel_t *in, pixel_t *out,
     cudaSafeCall(cudaMemcpy(d_max, &max, sizeof(pixel_t), cudaMemcpyHostToDevice));
     cudaSafeCall(cudaMemcpy(d_min, &min, sizeof(pixel_t), cudaMemcpyHostToDevice));
 
-    //min_max_cuda<<<block,grid>>>(out, nx, ny, d_min, d_max);
+    min_max_cuda<<<block,grid>>>(out, nx, ny, d_min, d_max);
 
     normalize_cuda<<<block,grid>>>(out, nx, ny, n, d_min, d_max);
 }
